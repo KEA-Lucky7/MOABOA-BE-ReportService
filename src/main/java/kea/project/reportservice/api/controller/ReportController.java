57e32ff6834fe0da57f4612a6b96c9e7f63084fd
wcorn <dsk08208@gmail.com>
2024-05-27 15:30:07 +0900

    [TEST] Auth test를 위한 test api

--- src/main/java/kea/project/reportservice/api/controller/ReportController.java
+++ src/main/java/kea/project/reportservice/api/controller/ReportController.java
@@ -1,17 +1,22 @@
 package kea.project.reportservice.api.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
 import kea.project.reportservice.api.controller.dto.response.GetReportResponse;
 import kea.project.reportservice.api.service.ReportService;
 import kea.project.reportservice.api.service.dto.GetReportServiceDto;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDate;
+import java.util.Enumeration;
 
 @RestController
 @RequiredArgsConstructor
+@Slf4j
 @RequestMapping("/reports")
 public class ReportController {
 
@@ -21,6 +26,23 @@ public class ReportController {
     public GetReportResponse GetReport(){
         //todo 인증 서비스 연결 전까지 일단 임의값 사용 추후 header에서 값 가져오는 걸로 변경
         Long memberId = 1L;
+        return reportService.getReport(GetReportServiceDto.of(memberId, LocalDate.now()));
+    }
+
+    @GetMapping("/compare/test")
+    public GetReportResponse GetReport(HttpServletRequest request){
+        Enumeration<String> headerNames = request.getHeaderNames();
+        while (headerNames.hasMoreElements()) {
+            String headerName = headerNames.nextElement();
+            String headerValue = request.getHeader(headerName);
+            log.info("Header Name: {}, Header Value: {}", headerName, headerValue);
+        }
+        Long memberId = 1L;
+        return reportService.getReport(GetReportServiceDto.of(memberId, LocalDate.now()));
+    }
+    @GetMapping("/compare/test2")
+    public GetReportResponse GetReport(@RequestHeader("USER") Long memberId){
+        log.info("member id = {}",memberId);
         return reportService.getReport(GetReportServiceDto.of(memberId, LocalDate.now()));
     }
 }
