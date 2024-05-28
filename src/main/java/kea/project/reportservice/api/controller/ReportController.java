0a028c02a2b3ab7d67f1a5cf842c2400e4af3843
Dongseok Kang <dsk08208@gmail.com>
2024-05-28 09:54:36 +0900

    [TEST] test api 삭제(#5)
    * [TEST] test api 삭제

--- src/main/java/kea/project/reportservice/api/controller/ReportController.java
+++ src/main/java/kea/project/reportservice/api/controller/ReportController.java
@@ -1,7 +1,5 @@
 package kea.project.reportservice.api.controller;
 
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
 import kea.project.reportservice.api.controller.dto.response.GetReportResponse;
 import kea.project.reportservice.api.service.ReportService;
 import kea.project.reportservice.api.service.dto.GetReportServiceDto;
@@ -26,29 +24,6 @@ public class ReportController {
     @GetMapping("/compare")
     public GetReportResponse GetReport(){
         //todo 인증 서비스 연결 전까지 일단 임의값 사용 추후 header에서 값 가져오는 걸로 변경
-        Long memberId = 1L;
-        return reportService.getReport(GetReportServiceDto.of(memberId, LocalDate.now()));
-    }
-
-    @GetMapping("/compare/test")
-    public GetReportResponse GetReport(HttpServletRequest request){
-        Enumeration<String> headerNames = request.getHeaderNames();
-        while (headerNames.hasMoreElements()) {
-            String headerName = headerNames.nextElement();
-            String headerValue = request.getHeader(headerName);
-            log.info("Header Name: {}, Header Value: {}", headerName, headerValue);
-        }
-        Long memberId = 1L;
-        return reportService.getReport(GetReportServiceDto.of(memberId, LocalDate.now()));
-    }
-    @GetMapping("/compare/test2")
-    public GetReportResponse GetReport(@RequestHeader("USER") Long memberId){
-        log.info("member id = {}",memberId);
-        return reportService.getReport(GetReportServiceDto.of(memberId, LocalDate.now()));
-    }
-    @GetMapping("/compare/test3")
-    public GetReportResponse GetReport(HttpServletResponse response){
-        response.setHeader("USER","1");
         Long memberId = 1L;
         return reportService.getReport(GetReportServiceDto.of(memberId, LocalDate.now()));
     }
